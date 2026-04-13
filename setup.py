e67ba5c11dc1252a092916bb15c490a4a58c171b
Jason Cipriani <jason.cipriani@gmail.com>
2026-04-12 23:24:10 -0400

    version bump 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="imagewriter-ii",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_namespace_packages(include="imagewriter*"),
     python_requires=">=3",
     install_requires=[
